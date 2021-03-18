f353f4dc1931e996d8789ddee4b7968f4aeb7cae
smunozc <salvador.munoz0109@gmail.com>
2021-03-18 11:44:51 +0100

    Correción de modelo de datos

--- docs/Arquitectura_del_Proyecto/Modelo_de_Datos/script-bd-inicial.sql
+++ docs/Arquitectura_del_Proyecto/Modelo_de_Datos/script-bd-inicial.sql
@@ -15,30 +15,32 @@ CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
 
 -- -----------------------------------------------------
+-- Table `mydb`.`USER`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`USER` (
+  `idUSER` INT NOT NULL AUTO_INCREMENT,
+  `token` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `profilePicture` BLOB NOT NULL,
+  `role` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idUSER`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `mydb`.`EXERCISE`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`EXERCISE` (
   `date` VARCHAR(45) NOT NULL,
   `calories_burned` VARCHAR(45) NOT NULL,
   `estimated_steps` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`date`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`USER`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`USER` (
-  `idUSER` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(45) NOT NULL,
-  `token` VARCHAR(45) NOT NULL,
-  `username` VARCHAR(45) NOT NULL,
-  `EXERCISE_date` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idUSER`, `EXERCISE_date`),
-  INDEX `fk_USER_EXERCISE1_idx` (`EXERCISE_date` ASC) VISIBLE,
-  CONSTRAINT `fk_USER_EXERCISE1`
-    FOREIGN KEY (`EXERCISE_date`)
-    REFERENCES `mydb`.`EXERCISE` (`date`)
+  `USER_idUSER` INT NOT NULL,
+  PRIMARY KEY (`date`, `USER_idUSER`),
+  INDEX `fk_EXERCISE_USER1_idx` (`USER_idUSER` ASC) VISIBLE,
+  CONSTRAINT `fk_EXERCISE_USER1`
+    FOREIGN KEY (`USER_idUSER`)
+    REFERENCES `mydb`.`USER` (`idUSER`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -51,29 +53,28 @@ CREATE TABLE IF NOT EXISTS `mydb`.`REWARD` (
   `idREWARD` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `type` VARCHAR(45) NOT NULL,
-  `imgUrl` VARCHAR(200) NOT NULL,
+  `badgeImg` BLOB NOT NULL,
   PRIMARY KEY (`idREWARD`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`USER_has_REWARD`
+-- Table `mydb`.`REWARD_has_USER`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`USER_has_REWARD` (
-  `USER_idUSER` INT NOT NULL,
-  `USER_EXERCISE_date` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`REWARD_has_USER` (
   `REWARD_idREWARD` INT NOT NULL,
-  PRIMARY KEY (`USER_idUSER`, `USER_EXERCISE_date`, `REWARD_idREWARD`),
-  INDEX `fk_USER_has_REWARD1_REWARD1_idx` (`REWARD_idREWARD` ASC) VISIBLE,
-  INDEX `fk_USER_has_REWARD1_USER1_idx` (`USER_idUSER` ASC, `USER_EXERCISE_date` ASC) VISIBLE,
-  CONSTRAINT `fk_USER_has_REWARD1_USER1`
-    FOREIGN KEY (`USER_idUSER` , `USER_EXERCISE_date`)
-    REFERENCES `mydb`.`USER` (`idUSER` , `EXERCISE_date`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_USER_has_REWARD1_REWARD1`
+  `USER_idUSER` INT NOT NULL,
+  PRIMARY KEY (`REWARD_idREWARD`, `USER_idUSER`),
+  INDEX `fk_REWARD_has_USER_USER1_idx` (`USER_idUSER` ASC) VISIBLE,
+  INDEX `fk_REWARD_has_USER_REWARD1_idx` (`REWARD_idREWARD` ASC) VISIBLE,
+  CONSTRAINT `fk_REWARD_has_USER_REWARD1`
     FOREIGN KEY (`REWARD_idREWARD`)
     REFERENCES `mydb`.`REWARD` (`idREWARD`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_REWARD_has_USER_USER1`
+    FOREIGN KEY (`USER_idUSER`)
+    REFERENCES `mydb`.`USER` (`idUSER`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
