21fbc485a8c6caf9f6fa50ff21c048897b348193
smunozc <salvador.munoz0109@gmail.com>
2021-03-30 18:45:21 +0200

    Añadido backend

---
+++ src/GFit-Analysis-Back/src/main/java/com/gfitanalysis/GFitAnalysisBack/GFitAnalysisBackApplication.java
@@ -0,0 +1,13 @@
+package com.gfitanalysis.GFitAnalysisBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GFitAnalysisBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GFitAnalysisBackApplication.class, args);
+	}
+
+}
---
+++ src/GFit-Analysis-Back/src/test/java/com/gfitanalysis/GFitAnalysisBack/GFitAnalysisBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfitanalysis.GFitAnalysisBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GFitAnalysisBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
