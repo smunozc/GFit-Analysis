f2b65284cec39afd3909443564d2b0242e5a5c47
smunozc <salvador.munoz0109@gmail.com>
2021-04-11 12:21:22 +0200

    dashboard parcialmente funcional

--- src/GFit-Analysis-Front/src/app/dashboard/dashboard.component.ts
+++ src/GFit-Analysis-Front/src/app/dashboard/dashboard.component.ts
@@ -10,9 +10,16 @@ import { ChartComponent } from '../chart/chart.component'
 })
 export class DashboardComponent implements OnInit {
 
+  totalSteps: number;
+  stepsSelected: boolean = true;
   constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    let aux = localStorage.getItem('totalStepsWeek')
+
+    if(aux !== null){
+      this.totalSteps = parseInt(aux);
+    }
   }
 
   logout(){
@@ -20,19 +27,35 @@ export class DashboardComponent implements OnInit {
   }
 
   selectButtonSteps(){
+    this.stepsSelected = true;
+
     let buttonSteps = document.getElementById('button-steps');
     let buttonCalories = document.getElementById('button-calories');
 
     buttonSteps.className = 'button-selected';
     buttonCalories.className = 'button';
+
+    let totalStepsTag = document.getElementById('totalSteps');
+    let totalCaloriesTag = document.getElementById('totalCalories');
+
+    totalStepsTag.className = 'totalDataVal-selected';
+    totalCaloriesTag.className = 'totalDataVal';
   }
 
   selectButtonCalories(){
+    this.stepsSelected = false;
+
     let buttonSteps = document.getElementById('button-steps');
     let buttonCalories = document.getElementById('button-calories');
 
     buttonSteps.className = 'button';
     buttonCalories.className = 'button-selected';
+
+    let totalStepsTag = document.getElementById('totalSteps');
+    let totalCaloriesTag = document.getElementById('totalCalories');
+
+    totalStepsTag.className = 'totalDataVal';
+    totalCaloriesTag.className = 'totalDataVal-selected';
   }
 
 }
